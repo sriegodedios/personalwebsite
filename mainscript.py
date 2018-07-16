3084d9fd5dedddedbae92df46494f7e3f1e7877c
Shane Riegodedios <shaner261996@gmail.com>
2018-07-16 13:33:52 -0500

    Made it work on the raspberry pi!

--- mainscript.py
+++ mainscript.py
@@ -1,10 +1,10 @@
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 #from django.setup import DJANGO_SETTINGS_MODULE
-from smbus2 import SMBus
+#from smbus2 import SMBus
 import threading
 import RPi.GPIO as GPIO
-import smbus
+import smbus as SMBus
 
 slaveAddress = 0x12
 numberInterruptPIN = 19
@@ -15,7 +15,7 @@ messageInterruptPIN = 12
 
 def djangoserver():
     application = get_wsgi_application()
-    call_command('runserver')
+    call_command('runserver','10.0.0.225:8000')
 
 webserver = threading.Thread(target=djangoserver)
 print("Server has been turned on!")
